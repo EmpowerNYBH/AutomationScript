f0d038bd92fcdc2ab3461aee429ffa3f6eb16ced
bmanh-nbc <129088711+bmanh-nbc@users.noreply.github.com>
2023-05-16 14:31:04 -0400

    Add project files.

---
+++ EmpowerNY_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerNY_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerNY_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9B29EAAC-A38A-4611-B7EE-833AD85F006C")]
+[assembly: AssemblyVersion("1.0.0.0")]
